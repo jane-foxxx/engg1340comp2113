6dd9408177149205f74c32df0d2057bebf847fa2
jane-foxxx <78797393+jane-foxxx@users.noreply.github.com>
2021-04-24 16:29:21 +0800

    Update ask_death.h

--- ask_death.h
+++ ask_death.h
@@ -10,7 +10,6 @@ using namespace std;
 
 //ask the player if play again
 void ask_death(int hp, int countdown, bool weapon){
-	print_death_reason(hp, countdown, weapon);
 	cout << endl;
 	cout << "        ******************************        " << endl;
 	cout << "              **** YOU DIED! ****             " << endl;
