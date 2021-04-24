207e6c3f004ab4000ac492d1e3eba6202f83e9f0
wxywxy0110 <78557081+wxywxy0110@users.noreply.github.com>
2021-04-24 11:09:57 +0800

    Create print_death.h

---
+++ print_death.h
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#define HEART   "\xE2\x99\xA5"
+#define DIAMOND "\xE2\x99\xA6"
+
+using namespace std;
+
+//print death announce for failure in the game
+void print_death(){
+	cout << "THANK YOU FOR YOUR CONTRIBUTION TO NIGHTMARE" << endl;
+	cout << "WE WISH YOU GOOD HELL :)" << endl;
+}
