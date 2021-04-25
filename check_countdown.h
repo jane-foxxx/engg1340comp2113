cbc17e24494e8711e293be3590a631a2435b0f98
jane-foxxx <78797393+jane-foxxx@users.noreply.github.com>
2021-04-26 00:21:01 +0800

    Update check_countdown.h

--- check_countdown.h
+++ check_countdown.h
@@ -3,15 +3,21 @@
 #include <string>
 #include <stdlib.h>
 #include <time.h>
+#include <iomanip>
 #define HEART   "\xE2\x99\xA5"
 #define DIAMOND "\xE2\x99\xA6"
 
+#include "function.h"
+
 using namespace std;
+
 
 //Time triger
 bool check_countdown(int countdown){
-	if (countdown > 70){
-		return false;
-	}
-	return true;
+    if (countdown > 50){
+        return false;
+    }
+    else{
+	    return true;
+    }
 }
