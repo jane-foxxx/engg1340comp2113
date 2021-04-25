f5721a5c773c3edde15722fb9d34a2306ad5d2df
jane-foxxx <78797393+jane-foxxx@users.noreply.github.com>
2021-04-26 00:28:26 +0800

    Update and rename print_death.h to print_death.cpp

--- print_death.cpp
+++ print_death.cpp
@@ -3,8 +3,11 @@
 #include <string>
 #include <stdlib.h>
 #include <time.h>
+#include <iomanip>
 #define HEART   "\xE2\x99\xA5"
 #define DIAMOND "\xE2\x99\xA6"
+
+#include "function.h"
 
 using namespace std;
 
