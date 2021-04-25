19c4a712308a769419af547dc2c3602ae53f10ca
jane-foxxx <78797393+jane-foxxx@users.noreply.github.com>
2021-04-26 00:28:01 +0800

    Update and rename print_current_value.h to print_current_value.cpp

--- print_current_value.cpp
+++ print_current_value.cpp
@@ -3,19 +3,22 @@
 #include <string>
 #include <stdlib.h>
 #include <time.h>
+#include <iomanip>
 #define HEART   "\xE2\x99\xA5"
 #define DIAMOND "\xE2\x99\xA6"
+
+#include "function.h"
 
 using namespace std;
 
 //print the current states of the player so that the player can make more choice
 void print_current_value(int hp,int countdown, bool weapon){
-	cout << endl;
-	cout << "-------Current Condition-------" << endl;
+    cout << endl;
+    cout << "-------Current Condition-------" << endl;
 	//hp
 	cout << "|       health point: " << hp <<"       |" << endl;
 	//count down
-	cout << "|       time left: " << (70-countdown) << " min     |" << endl;
+	cout << "|       time left: " << (50-countdown) << " min     |" << endl;
 	//weapon
 	if(weapon == true){
 		cout << "|       weapon: stick         |" << endl;
@@ -25,3 +28,4 @@ void print_current_value(int hp,int countdown, bool weapon){
 	}
 	cout << "-------------------------------" << endl;
 }
+
