00b0e7541ec637a41ab81edc19322d3dabc55386
jane-foxxx <78797393+jane-foxxx@users.noreply.github.com>
2021-04-26 00:21:35 +0800

    Update fightguardian.h

--- fightguardian.h
+++ fightguardian.h
@@ -3,18 +3,25 @@
 #include <string>
 #include <stdlib.h>
 #include <time.h>
+#include <iomanip>
 #define HEART   "\xE2\x99\xA5"
 #define DIAMOND "\xE2\x99\xA6"
+
+#include "function.h"
 
 using namespace std;
 
 //the action of fighting with the guardian
 //true means the player defeat the guardian and survive, and health point will decrease by 3
 //false means the player is defeated and die
-bool fightguardian(bool weapon, int &hp){
+bool fightguardian(bool weapon, int &hp, int &countdown){
 	if(hp > 5 && weapon == true){
-	    hp -= 3;
+	   	hp -= 3;
+		countdown += 10;
 		return true;
 	}
-	return false;
+	else{
+		return false;
+	}
 }
+
